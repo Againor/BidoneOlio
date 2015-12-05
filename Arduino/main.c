c7707204dc9ef832dc127ad0c17dea61a1b78310
TheGameBoyITA <dagameboyita@gmail.com>
2015-12-05 09:09:49 +0100

    Implementato NewPing e LowPower

--- Arduino/main.c
+++ Arduino/main.c
@@ -1,44 +1,37 @@
-const int echo = 8;
-const int trig = 9;
-const int pieno = 10;
-const int vuoto = 20;
-const int rosso = 11;
-const int verde = 10;
- 
+<<<<<<< HEAD
+#include<NewPing.h>
+#include<LowPower.h>
+
+#define echo  8;
+#define trig  9;
+#define pieno 200;
+#define vuoto 2
+#define rosso 11;
+#define verde 10;
+
+NewPing sonar(trig, echo, pieno);
 void setup() {
-  Serial.begin ( 9600 );
-  pinMode ( echo, INPUT );
-  pinMode ( trig, OUTPUT );
-  pinMode ( rosso, OUTPUT);
-  pinMode ( verde, OUTPUT);
+  Serial.begin(9600);
 }
 
 void loop() {
-  digitalWrite ( trig, LOW );
-  digitalWrite ( trig, HIGH );
-  delay ( 100 );
-  digitalWrite ( trig, LOW );
-  
-  long time = pulseIn ( echo, HIGH );
-  long distanza = 0.034 * time / 2;
-  
-  if ( distanza > 3000 ){
-    Serial.println ( "Fuori Portata!" );
-  } else if ( distanza > vuoto ){
-    digitalWrite ( rosso, LOW );
-    digitalWrite ( verde, HIGH );
-    Serial.print ( distanza );
-    Serial.println ( "cm   " );
-  } else if ( distanza < vuoto && distanza > pieno ){
-    digitalWrite ( rosso, HIGH );
-    digitalWrite ( verde, HIGH );
-    Serial.print ( distanza );
-    Serial.println ( "cm   " );
-  } else if ( distanza < pieno ){
-    digitalWrite ( rosso, HIGH );
-    digitalWrite ( verde, LOW );
-    Serial.print ( distanza );
-    Serial.println ( "cm   " );
-  }
-  delay(250); 
+    int distanza = sonar.ping() / US_ROUNDTRIP_CM;
+    if ( distanza > 3000 ){
+        Serial.println ( "Fuori Portata!" );
+    } else if ( distanza > vuoto ){
+        digitalWrite(rosso, LOW);
+        digitalWrite(verde, HIGH);
+    } else if ( distanza < vuoto && distanza > pieno ){
+        digitalWrite(rosso, HIGH);
+        digitalWrite(verde, HIGH);
+    } else if ( distanza < pieno ){
+        digitalWrite(rosso, HIGH);
+        digitalWrite(verde, LOW);
+    }
+    Serial.println(distanza + " cm");
+    //1h = 3600s / 8s = 450
+    //TODO Trovare un modo migliore di far diminuire la corrente nello sleep
+    for(int i; i < 450; i++){
+        LowPower.powerDown(SLEEP_8S, ADC_CONTROL_OFF, BOD_OFF);
+    }
 }
